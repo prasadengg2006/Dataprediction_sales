8a2ab47dc1850d9704a588e6776e1ef9f6e04d22
prasadengg2006 <prasadengg2006@gmail.com>
2025-01-03 16:56:02 +0530

    Add files via upload

--- datapreporcessingcs.py
+++ datapreporcessingcs.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from pandas import Series,DataFrame
 import numpy as np
 import matplotlib.pyplot as plt
-
+import plotly.express as px
 from prophet import Prophet
 
 import warnings
@@ -103,19 +103,26 @@ ari_forecast=ari_result.get_forecast(steps=len(test))
 ari_fore_values=ari_forecast.predicted_mean
 ari_predict=ari_result.predict(start=len(train),end=len(train)+len(test)-1, dynamic=True)
 ari_predict.index=test.index
-concatenated_series=pd.concat([test,ari_predict],axis=1)
+
+concatenated_series=pd.concat([test,ari_predict],axis=1) 
 concatenated_series.columns=['Actual','Predicted']
-st.write(concatenated_series)
+#st.write(concatenated_series)
 #cs=concatenated_series.plot(title="Actual vs Predicted")
 #concatenated_series.plot(figsize=(12,6))
 #fig_actual_predict=plt.figure(figsize=(12,6))
 
-#plt.xlabel('ordered date')
-#plt.xlabel('quantity sold')
-#plt.title('Actual vs predicted')
-st.pyplot(concatenated_series.plot.line(stacked=True).figure)
+plt.xlabel('ordered date')
+plt.xlabel('quantity sold')
+plt.title('Actual vs predicted')
+fig=px.line(concatenated_series)
 
-#Evaluate the model
+#fig=px.line(concatenated_series,x='Predicted',y='Actual',labels={'y':'Actual'})
+#fig.add_scatter(x=concatenated_series['Actual'],y=concatenated_series['Predicted'], mode='lines',name='Predicted')
+fig.update_layout(title='Actual Vs Predicted', xaxis_title='Date', yaxis_title='Value')
+st.plotly_chart(fig,use_container_width=True)
+
+
+#Evaluate the mode
 mae=mean_absolute_error(test,ari_fore_values)
 mse=mean_squared_error(test,ari_fore_values)
 rmse=np.sqrt(mse)
@@ -144,7 +151,11 @@ st.write(sari_predict)
 concatenated_sarima=pd.concat([test,sari_predict],axis=1)
 concatenated_sarima.columns=['Actual','Predicted']
 st.write(concatenated_sarima)
-st.pyplot(concatenated_sarima.plot.line(stacked=True).figure)
+fig=px.line(concatenated_sarima)
+#fig.add_scatter(x=concatenated_sarima['order_date'],y=concatenated_sarima['Predicted'], mode='lines',name='Predicted')
+fig.update_layout(title='Actual Vs Predicted', xaxis_title='Date', yaxis_title='Value')
+st.plotly_chart(fig,use_container_width=True)
+
 
 one_pizza_prophet=pd.DataFrame({'ds':one_pizza.index,'y':one_pizza.values})
 st.write(one_pizza_prophet.head())
@@ -169,5 +180,65 @@ st.write(f'prophet rmse :{rmse}')
 
 concat_prophet=pd.concat([pr_test,pr_forecast[['yhat']].iloc[-len(pr_test):]],axis=1)
 concat_prophet.columns=['date','Actual','Predicted']
-concat_prophet.set_index('date',inplace=True)
+#concat_prophet.set_index('date',inplace=True)
 st.write(concat_prophet)
+
+fig=px.line(concat_prophet,x='date',y='Actual',labels={'y':'Actual'})
+fig.add_scatter(x=concat_prophet['date'],y=concat_prophet['Predicted'], mode='lines',name='Predicted')
+fig.update_layout(title='Actual Vs Predicted', xaxis_title='Date', yaxis_title='Value')
+st.plotly_chart(fig,use_container_width=True)
+
+pizza_df=pizza_df[['order_date','pizza_name','quantity']]
+sales_summary=pizza_df.groupby(['order_date','pizza_name'])['quantity'].sum().reset_index()
+st.write(sales_summary.head())
+
+sales_pivot=sales_summary.pivot(index='order_date',columns='pizza_name',values='quantity').fillna(0)
+st.write(sales_pivot)
+arima_models={}
+for pizza_name in sales_pivot.columns:
+    try:
+        model=ARIMA(sales_pivot[pizza_name],order=(1,1,0))
+        model_fit=model.fit()
+        arima_models[pizza_name]=model_fit
+    except:
+        print(f'Arima model for {pizza_name} failed for fit')
+
+#generate predictions for one week 
+prediction_days=7
+predictions_arima={}
+st.write(arima_models)
+for pizza_name,model in arima_models.items():
+    predictions_arima[pizza_name]=model.predict(start=len(sales_pivot),end=len(sales_pivot)+ prediction_days-1)
+
+predictions_df=pd.DataFrame(predictions_arima)
+predictions_df.index=pd.date_range(start=sales_pivot.index[-1],periods=prediction_days,freq='D')
+
+ingred_df=ingredients_df[['pizza_name','pizza_ingredients','Items_Qty_In_Grams']]
+ingred_df.rename(columns={'Items_Qty_In_Grams':'items_qty'},inplace=True)
+
+st.write(predictions_df)
+st.write(ingred_df.head())
+
+ingridient_quantities={}
+
+for pizza_name in predictions_df.columns:
+    predicted_qty=predictions_df[pizza_name].sum()
+    pizza_ingredients=ingred_df[ingred_df['pizza_name']==pizza_name]  
+    for index, row in pizza_ingredients.iterrows():
+     ingredient=row['pizza_ingredients']
+     ingredient_qty=row['items_qty']
+     required_qty=predicted_qty*ingredient_qty
+
+     if ingredient not in ingridient_quantities:
+         ingridient_quantities[ingredient]=0
+     ingridient_quantities[ingredient]=required_qty  
+
+
+ingridient_requirements_df=pd.DataFrame.from_dict(ingridient_quantities,orient='index',columns=['required_quantity']) 
+st.write(ingridient_requirements_df)    
+
+
+purchase_order_df=ingridient_requirements_df.copy()
+purchase_order_df['units']='grams'
+purchase_order_df=purchase_order_df.rename(columns={'required_Quantity':'quantity'})
+st.write(purchase_order_df)
